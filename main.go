c8867763693e0ca11180326f32130569c03b183b
Ivan Sinyavsky <ivan-tg@mail.ru>
2021-02-10 14:21:07 +0300

    проведен рефакторинг

--- main.go
+++ main.go
@@ -4,44 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
+	srv := &http.Server{
+		Addr:    ":4000",
+		Handler: routes(),
+	}
+	fmt.Println("Запуск сервера на :4000")
+	err := srv.ListenAndServe()
 	log.Fatal(err)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Добро пожаловать"))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Отображение заметки с ID %d...", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод запрещен", 405)
-		return
-	}
-
-	w.Write([]byte("Создание заметки"))
 }
